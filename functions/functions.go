4456686da2a2abb2ad0f183621a16fd6fcdd20da
Jeff Nelson <longshot902@gmail.com>
2024-12-02 11:17:58 -0600

    day2

--- functions/functions.go
+++ functions/functions.go
@@ -1,9 +1,23 @@
 package functions
 
+import (
+	"strconv"
+)
+
 // AbsInt Return absolute value
 func AbsInt(n int) int {
 	if n < 0 {
 		return -n
 	}
 	return n
+}
+
+func StringToInts(s []string) []int {
+	ints := make([]int, len(s))
+
+	for i, s := range s {
+		num, _ := strconv.Atoi(s)
+		ints[i] = num
+	}
+	return ints
 }
